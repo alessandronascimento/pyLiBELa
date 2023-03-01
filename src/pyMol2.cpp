8b3547f62b190bff1a27175468d075f7f4bb8633
alessandronascimento <al.s.nascimento@gmail.com>
2023-03-01 16:17:36 -0300

    Add files via upload

--- src/pyMol2.cpp
+++ src/pyMol2.cpp
@@ -946,4 +946,10 @@ BOOST_PYTHON_MODULE(pyMol2)
         .def("find_longest_axis", & Mol2::find_longest_axis)
         .def("distance", & Mol2::distance)
     ;
+    class_<atom_param>("atom_param")
+        .def_readwrite("type", & Mol2::atom_param::type)
+        .def_readwrite("radius", & Mol2::atom_param::radius)
+        .def_readwrite("epsilon", & Mol2::atom_param::epsilon)
+        .def_readwrite("mass", & Mol2::atom_param::mass)
+    ;
 }
