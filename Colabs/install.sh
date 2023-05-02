9611e846d1038539f73cd1e50c3083435df8b2c7
Caio Oliveira <caio.dejesusoliveira@usp.br>
2023-05-02 11:57:44 -0300

    Update install.sh

--- Colabs/install.sh
+++ Colabs/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-apt-get install libboost-python1.71-dev openbabel libopenbabel-dev python-dev-is-python3 zlib1g-dev libeigen3-dev libgsl-dev libnlopt-cxx-dev libgsl-dev
 make clean
 make FILES
 make -j4
