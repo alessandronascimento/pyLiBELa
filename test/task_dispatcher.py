8a56e757e57da3213b8c0c30cd09a10f1754aed3
Alex Souza Carvalho <alex98sc@usp.br>
2023-10-07 13:48:59 -0300

    finished grid processing solution

--- test/task_dispatcher.py
+++ test/task_dispatcher.py
@@ -1,7 +1,17 @@
 import sys
+import os
 
 def main():
     
+    dir = sys.argv[1] 
+    src = "/home/fac001/workingproj/targets_refined_set"
+    out = "/home/fac001/workingproj/grids" 
+
+    arg1 = f"{src}/{dir}/{dir}_rec.mol2.gz"
+    arg2 = f"{src}/{dir}/{dir}_ligand.mol2.gz"
+    arg3 = f"{out}/{dir}"
+
+    os.system(f"./process_grids {arg1} {arg2} {arg3}")
 
 if __name__ == "__main__":
     main()
