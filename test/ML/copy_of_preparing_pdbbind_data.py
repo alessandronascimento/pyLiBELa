c9c696e0db942421589c45a40b6fa7c140febb7d
AlexSouzaCarvalho <alex98sc@usp.br>
2024-04-03 14:49:53 -0300

    small fix

--- test/ML/copy_of_preparing_pdbbind_data.py
+++ test/ML/copy_of_preparing_pdbbind_data.py
@@ -14,7 +14,7 @@ physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 def read_pdbbind_data():
-#@title Getting list of targets with grids
+    #@title Getting list of targets with grids
     targets_ok = []
     target_ok_file = open(f'{path_to_pdbbind}/scripts/targets_ok.dat', 'r')
     for line in target_ok_file:
@@ -22,29 +22,28 @@ def read_pdbbind_data():
         targets_ok.append(line2[0])
     target_ok_file.close()
 
-#@title Reading PDBBind v2020 data
+    #@title Reading PDBBind v2020 data
     binding_targets = []             # pdb id
     binding_years = []               # year of the structure
     binding_resolution = []          # resolution
     binding_score = []               # -logKd/Ki
 
-    binding_file = open(f'{path_to_pdbbind}/index/INDEX_refined_data.2020', 'r')
-    for line in binding_file:
-        line2 = line.split()
-        if line2[0][0] != '#':
+    with open(f'{path_to_pdbbind}/index/INDEX_refined_data.2020', 'r') as binding_file:
+        for line in binding_file:
+            line2 = line.split()
+            if line2[0][0] == '#': continue
             if line2[0] not in targets_ok: continue
 
-        binding_targets.append(line2[0])
-        if (line2[1] == 'NMR'):
-            binding_resolution.append(0.00)
-        else:
-            binding_resolution.append(float(line2[1]))
+            binding_targets.append(line2[0])
+            if (line2[1] == 'NMR'):
+                binding_resolution.append(0.00)
+            else:
+                binding_resolution.append(float(line2[1]))
+                
             binding_years.append(int(line2[2]))
             binding_score.append(float(line2[3]))
-    binding_file.close()
 
     print('Binding data found for %d valid targets' % len(binding_targets))
-
     return binding_targets, binding_score
 
 #@title Auxiliary functions
