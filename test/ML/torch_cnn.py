7d78be405e71e84c2ab796ae84a37e7fad7bf7e6
Alex <alex98sc@usp.br>
2024-04-15 09:44:03 -0300

    adding read_pdbbind_data and process_file modules
    Untested iteration of read_pdbind_data() and process_file() functions as pyTorch modules.

---
+++ test/ML/torch_cnn.py
@@ -0,0 +1,56 @@
+import torch
+import numpy as np
+from pathlib import Path
+
+path_to_pdbbind = "/data/home/alessandro/PDBbind_v2020"
+LOG_DIR = "./log"
+
+def read_pdbbind_data():
+    #@title Getting list of targets with grids
+    targets_ok = []
+    target_ok_file = open(f'{path_to_pdbbind}/scripts/targets_ok.dat', 'r')
+    for line in target_ok_file:
+        line2 = line.split()
+        targets_ok.append(line2[0])
+    target_ok_file.close()
+
+    #@title Reading PDBBind v2020 data
+    binding_targets = []             # pdb id
+    binding_years = []               # year of the structure
+    binding_resolution = []          # resolution
+    binding_score = []               # -logKd/Ki
+
+    with open(f'{path_to_pdbbind}/index/INDEX_refined_data.2020', 'r') as binding_file:
+        for line in binding_file:
+            line2 = line.split()
+            if line2[0][0] == '#': continue
+            if line2[0] not in targets_ok: continue
+
+            binding_targets.append(line2[0])
+            if (line2[1] == 'NMR'):
+                binding_resolution.append(0.00)
+            else:
+                binding_resolution.append(float(line2[1]))
+                
+            binding_years.append(int(line2[2]))
+            binding_score.append(float(line2[3]))
+
+    print('Binding data found for %d valid targets' % len(binding_targets))
+    return binding_targets, binding_score
+
+def process_file(file_path):
+    as_path = Path(file_path)
+    if not as_path.exists():
+        raise IOError(f"File path '{as_path}' does not exist")
+
+    with open(file_path, 'rb') as f:
+        data = np.load(f)
+        data = torch.frombuffer(data, dtype=torch.float64).reshape(60, 60, 60, 6)
+
+    return data
+
+def test():
+    process_file()
+
+if __name__ == '__main__':
+    test()
