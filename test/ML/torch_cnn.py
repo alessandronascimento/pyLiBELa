f1590d4856f14c7b92b261665e2561b666098678
AlexSouzaCarvalho <alex98sc@usp.br>
2024-04-24 14:11:10 -0300

    feature torch model
    Runnable torch cnn model, but requires too much GPU memory. Sample
    weights missing

--- test/ML/torch_cnn.py
+++ test/ML/torch_cnn.py
@@ -1,6 +1,7 @@
 import torch
+import numpy as np
 from math import floor
-from typing import OrderedDict
+from typing import OrderedDict, Literal, Optional
 from torch.utils.data import Dataset, DataLoader
 from torch.optim import Optimizer
 from torch import nn
@@ -49,14 +50,16 @@ def process_file(file_path):
         raise IOError(f"File path '{as_path}' does not exist")
 
     with open(file_path, 'rb') as f:
-        data = torch.frombuffer(f.read(), dtype=torch.float64).reshape(3, 60, 60, 60)
+        data = torch.tensor(np.frombuffer(f.read(), dtype=np.float64).reshape((3,60,60,60)), dtype=torch.float32)
+        
     return data
 
 
 class GridDataset(Dataset):
-    def __init__(self, targets_list, scores_list) -> None:
+    def __init__(self, targets_list, scores_list, device) -> None:
         self.targets = targets_list
         self.scores = scores_list 
+        self.device = device
         self._rec_idx = None
         # Sample weights
         self.weight_normal = 11/2
@@ -84,9 +87,20 @@ class GridDataset(Dataset):
             weight = self.weight_decoy
 
         self._lig = process_file(self._file)
-        return torch.cat((self._rec, self._lig), dim=0), self.scores[grid_idx], weight
 
-def create_datasets(max_targets=None):
+        out_tuple = (
+            torch.cat((self._rec, self._lig), dim=0).to(self.device),
+            torch.tensor(self.scores[grid_idx]).to(self.device),
+            torch.tensor(weight).to(self.device)
+        )
+
+        # return torch.cat((self._rec, self._lig), dim=0).to(self.device), self.scores[grid_idx], weight
+        return out_tuple
+
+#TODO look into with num_workers and prefetch
+def create_datasets(device : Literal['mps', 'cuda', 'cpu'],
+                    batch_size : int,
+                    max_targets : Optional[int] = None):
     
     binding_targets, binding_score = read_pdbbind_data()
 
@@ -96,9 +110,12 @@ def create_datasets(max_targets=None):
                                                                           shuffle=True)
     train_targets, valid_targets, train_scores, valid_scores = train_test_split(train_targets, train_scores, train_size=0.8)
 
-    train_dataset = GridDataset(train_targets, train_scores) 
-    test_dataset = GridDataset(test_targets, test_scores) 
-    valid_dataset = GridDataset(valid_targets, valid_scores) 
+    train_dataset = DataLoader(GridDataset(train_targets, train_scores, device),
+                               batch_size=batch_size)
+    test_dataset = DataLoader(GridDataset(test_targets, test_scores, device),
+                               batch_size=batch_size)
+    valid_dataset = DataLoader(GridDataset(valid_targets, valid_scores, device),
+                               batch_size=batch_size)
 
     return train_dataset, test_dataset, valid_dataset
 
@@ -117,25 +134,26 @@ class CNNModel(nn.Module):
         self._current_shape = input_shape 
         self._old_out_channels = 0
         self.model_stack = nn.Sequential(OrderedDict([
-            ('conv1',   self.make_conv3d(out_channels=32,
+            ('conv1',    self.make_conv3d(out_channels=32,
                                         kernel_size=3,
                                         in_channels=input_shape[0])),
             ('maxpool1', self.make_maxpool3d(kernel_size=2)),
-            ('conv2', self.make_conv3d(out_channels=64, kernel_size=3)),
-            ('conv3', self.make_conv3d(out_channels=64, kernel_size=2)),
+            ('conv2',    self.make_conv3d(out_channels=64, kernel_size=3)),
+            ('conv3',    self.make_conv3d(out_channels=64, kernel_size=2)),
             ('maxpool2', self.make_maxpool3d(kernel_size=2)),
-            ('conv4', self.make_conv3d(out_channels=128, kernel_size=2)),
-            ('conv5', self.make_conv3d(out_channels=128, kernel_size=2)),
-            ('flat', self.make_flatten()),
-            ('linear1', self.make_linear(units=128)),
-            ('activation1', nn.ReLU()),
-            ('linear2', self.make_linear(units=64)),
-            ('activation2', nn.ReLU()),
-            ('out', self.make_linear(units=1))
+            ('conv4',    self.make_conv3d(out_channels=128, kernel_size=2)),
+            ('conv5',    self.make_conv3d(out_channels=128, kernel_size=2)),
+            ('flat',     self.make_flatten()),
+            ('linear1',  self.make_linear(units=8)),
+            ('activ1', nn.ReLU()),
+            ('linear2',  self.make_linear(units=4)),
+            ('activ2', nn.ReLU()),
+            ('out',      self.make_linear(units=1))
             ]))
 
     def forward(self, x):
         x = self.model_stack(x)
+        x = torch.flatten(x)
         return x
 
     def make_conv3d(self,
@@ -202,7 +220,7 @@ class CNNModel(nn.Module):
         """
         if not isinstance(self._current_shape, tuple): raise ValueError("Current shape must be multidimensional")
 
-        self._current_shape = self._current_shape[0] * self._current_shape[1] * self._current_shape[2]
+        self._current_shape = self._current_shape[0] * self._current_shape[1] * self._current_shape[2] * self._current_shape[3]
         return nn.Flatten()
 
     def make_linear(self, units) -> nn.Linear:
@@ -250,10 +268,11 @@ def train_model(model : nn.Module,
                 valid_data: DataLoader,
                 optimizer : Optimizer, 
                 epochs : int,
-                loss_fn):
+                loss_fn : nn.MSELoss):
 
-    batch_size : int = train_data.batch_size
-    size = len(train_data)
+    batch_size = train_data.batch_size 
+    if (batch_size := train_data.batch_size) is None:
+        raise ValueError("Training DataLoader must have a batch size")
 
     for epoch in range(epochs):
         # TRAINING STEP -----------------------------
@@ -261,29 +280,32 @@ def train_model(model : nn.Module,
         running_loss = 0
         print(f"EPOCH {epoch+1}")
         model.train()
-        for batch, (X, y) in enumerate(train_data):
+        for batch, (X, y, w) in enumerate(train_data):
             pred = model(X)
             loss = loss_fn(pred, y)
-
+            # loss = loss * w.mean() # sample weight
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            optimizer.zero_grad()
             running_loss += loss.item()
 
-            if batch % batch_size == 0:
+            if batch % 100 == 0:
                 avg_loss = running_loss / batch_size # loss per batch
-                print(f"batch {batch+1} loss: {avg_loss}")
+                print(f"batch #{batch} loss: {avg_loss:.4f}\r")
                 running_loss = 0
 
         # VALIDATION STEP ---------------------------
         running_vloss = 0 
         model.eval()
         with torch.no_grad():
-            for Xv, yv in valid_data:
+            for Xv, yv, wv in valid_data:
                 vpred = model(Xv)
                 vloss = loss_fn(vpred, yv)
+                # vloss *= wv
+
                 running_vloss += vloss.item()
 
+        size = len(valid_data)
         avg_vloss = running_vloss / (size + 1)
         print(f"LOSS train {avg_loss} valid {avg_vloss}")
 
@@ -292,17 +314,17 @@ def test():
 
     #data = process_file(f'{path_to_pdbbind}/targets/8gpb/grid_30_0.5_SF0/McGrid_lig.grid')
     #print(data)
-    targets, scores = read_pdbbind_data()
     # dataset = GridDataset(targets[:23], scores[:23])
-    cnn = CNNModel((6,60,60,60)).to(get_device())
+
+    device = get_device()
+    cnn = CNNModel((6,60,60,60)).to(device)
     learning_rate = 1e-3
     batch_size = 5
     epochs = 10
     loss_fn = nn.MSELoss()
     optimizer = torch.optim.Adam(cnn.parameters(), lr=learning_rate)
-    train, _, valid = create_datasets(max_targets=100)
-    train_d, valid_d = DataLoader(train, batch_size=batch_size), DataLoader(valid, batch_size=batch_size)
-    train_model(cnn, train_d, valid_d, optimizer, epochs, loss_fn)
+    train, _, valid = create_datasets(device, 1, max_targets=100)
+    train_model(cnn, train, valid, optimizer, epochs, loss_fn)
 
 
 if __name__ == '__main__':
