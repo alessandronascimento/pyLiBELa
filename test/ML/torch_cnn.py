f47694eb1d96422736ba088562dcd33532615bda
AlexSouzaCarvalho <alex98sc@usp.br>
2024-05-06 09:51:38 -0300

    added fetures + cleaned code + wandb support
    Added missing ReLU layers to model.
    Created simple solution to programmatically set shape in model.
    Cleaned code comments.
    Created simple progress bar during model training.
    Added wandb support.
    Added wandb files to .gitignore

--- test/ML/torch_cnn.py
+++ test/ML/torch_cnn.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import wandb
 from math import floor
 from typing import OrderedDict, Literal, Optional
 from torch.utils.data import Dataset, DataLoader
@@ -101,6 +102,10 @@ class GridDataset(Dataset):
 def create_datasets(device : Literal['mps', 'cuda', 'cpu'],
                     batch_size : int,
                     max_targets : Optional[int] = None):
+    '''
+    Returns Dataloaders for train dataset, test dataset 
+    and validation dataset respectively
+    '''
     
     binding_targets, binding_score = read_pdbbind_data()
 
@@ -130,149 +135,48 @@ class CNNModel(nn.Module):
 
     def __init__(self, input_shape : tuple[int, int, int, int]) -> None:
         super().__init__()
-        self.flatten = nn.Flatten()
-        self._current_shape = input_shape 
-        self._old_out_channels = 0
-        self.model_stack = nn.Sequential(OrderedDict([
-            # ('conv1',    self.make_conv3d(out_channels=32,
-            #                             kernel_size=3,
-            #                             in_channels=input_shape[0])),
+        self.conv_stack = nn.Sequential(OrderedDict([
             ('conv1',    nn.Conv3d(in_channels=input_shape[0], out_channels=32, kernel_size=3)),
-            # ('maxpool1', self.make_maxpool3d(kernel_size=2)),
+            ('relu1',    nn.ReLU()),
             ('maxpool1', nn.MaxPool3d(kernel_size=2)),
-            # ('conv2',    self.make_conv3d(out_channels=64, kernel_size=3)),
+
             ('conv2',    nn.Conv3d(in_channels=32, out_channels=64, kernel_size=3)),
-            # ('conv3',    self.make_conv3d(out_channels=64, kernel_size=2)),
+            ('relu2',    nn.ReLU()),
+
             ('conv3', nn. Conv3d(in_channels=64, out_channels=64, kernel_size=2)),
-            # ('maxpool2', self.make_maxpool3d(kernel_size=2)),
+            ('relu3' , nn.ReLU()),
             ('maxpool2', nn.MaxPool3d(kernel_size=2)),
-            # ('conv4',    self.make_conv3d(out_channels=128, kernel_size=2)),
+
             ('conv4', nn.Conv3d(in_channels=64, out_channels=128, kernel_size=2)),
-            # ('conv5',    self.make_conv3d(out_channels=128, kernel_size=2)),
+            ('relu4', nn.ReLU()),
+
             ('conv5', nn.Conv3d(in_channels=128, out_channels=128, kernel_size=2)),
-            # ('flat',     self.make_flatten()),
+            ('relu5', nn.ReLU()),
+
             ('flat', nn.Flatten()),
-            # ('linear1',  self.make_linear(units=8)),
-            ('linear1', nn.Linear(in_features=170368, out_features=128)),
-            ('activ1', nn.ReLU()),
-            # ('linear2',  self.make_linear(units=4)),
-            ('linear2', nn.Linear(in_features=128, out_features=64)),
-            ('activ2', nn.ReLU()),
-            # ('out',      self.make_linear(units=1))
-            ('out', nn.Linear(in_features=64, out_features=1))
             ]))
 
+        self.linear_size = self.conv_stack(torch.zeros([1]+list(input_shape))).shape[1]
+
+        self.dense_stack = nn.Sequential(OrderedDict([
+            ('linear1', nn.Linear(in_features=self.linear_size, out_features=128)),
+            ('activ1', nn.ReLU()),
+
+            ('batch_norm', nn.BatchNorm1d(128)),
+            ('dropout', nn.Dropout(0.5)),
+
+            ('linear2', nn.Linear(in_features=128, out_features=64)),
+            ('activ2', nn.ReLU()),
+
+            ('out', nn.Linear(in_features=64, out_features=1))
+            ]))
+    
     def forward(self, x):
-        x = self.model_stack(x)
+        x = self.conv_stack(x)
+        x = self.dense_stack(x)
         x = torch.flatten(x)
         return x
 
-    def make_conv3d(self,
-                  out_channels,
-                  kernel_size,
-                  in_channels=None,
-                  padding=0,
-                  stride=1,
-                  dilation=1) -> nn.Conv3d:
-        """
-        Returns a Conv3d layer handling input shape automatically 
-        """
-
-        if in_channels is None: in_channels = self._old_out_channels
-        conv = nn.Conv3d(in_channels=in_channels, 
-                         out_channels=out_channels, 
-                         kernel_size=kernel_size,
-                         padding=padding,
-                         stride=stride,
-                         dilation=dilation)
-
-        if not isinstance(self._current_shape, tuple): raise ValueError("Current shape must be multidimensional")
-
-        dims = [self._current_shape[1], self._current_shape[2], self._current_shape[3]]
-        out = self._out_dims(dims, 
-                              kernel_size=kernel_size,
-                              padding=padding,
-                              stride=stride,
-                              dilation=dilation)
-
-        self._old_out_channels = out_channels
-        self._current_shape = (out_channels, out[0], out[1], out[2])
-
-        return conv
-
-    def make_maxpool3d(self,
-                       kernel_size,
-                       stride=1,
-                       padding=0,
-                       dilation=1) -> nn.MaxPool3d:
-        """
-        Returns a MaxPool3d layer handling input shape automatically 
-        """
-
-        if not isinstance(self._current_shape, tuple): raise ValueError("Current shape must be multidimensional")
-
-        maxpool = nn.MaxPool3d(kernel_size=kernel_size,
-                               stride=stride,
-                               padding=padding,
-                               dilation=dilation)
-
-        dims = [self._current_shape[1], self._current_shape[2], self._current_shape[3]]
-        out = self._out_dims(dims, 
-                              kernel_size=kernel_size,
-                              padding=padding,
-                              stride=stride,
-                              dilation=dilation)
-        self._current_shape = (self._old_out_channels, out[0], out[1], out[2])
-        return maxpool 
-
-    def make_flatten(self) -> nn.Flatten:
-        """
-        Returns a Flatten layer handling shape automatically 
-        """
-        if not isinstance(self._current_shape, tuple): raise ValueError("Current shape must be multidimensional")
-
-        self._current_shape = self._current_shape[0] * self._current_shape[1] * self._current_shape[2] * self._current_shape[3]
-        return nn.Flatten()
-
-    def make_linear(self, units) -> nn.Linear:
-        """
-        Returns a Linear layer handling input shape automatically 
-        """
-
-        if not isinstance(self._current_shape, int): raise ValueError("Shape must be flat")
-
-        lin = nn.Linear(in_features=self._current_shape, out_features=units)
-
-        self._current_shape = units
-        return lin
-
-    def _make_uniform(self, value : int | list[int]) -> tuple[list[int], list[int]]:
-        if isinstance(value, int):
-            return [value,], [0, 0, 0]
-        else: return value, [0, 1, 2]
-
-
-    def _out_dims(self, dims : list[int],
-                   kernel_size : int | list[int],
-                   padding : int | list[int] = 0,
-                   stride : int | list[int] = 1,
-                   dilation : int | list[int] = 1) -> tuple[int, int, int]:
-        """
-        Helper function that returns a tuple with the shape 
-        information (excluding channels) as outputted by a Conv3d 
-        or MaxPool3d layer to be used by next layer.
-        """
-
-        kernel_size, kernel_shape = self._make_uniform(kernel_size)
-        padding, padding_shape = self._make_uniform(padding)
-        stride, stride_shape = self._make_uniform(stride)
-        dilation, dilation_shape = self._make_uniform(dilation) 
-       
-        def reshape(i : int) -> int:
-            return floor(1 + (dims[i] + 2*padding[padding_shape[i]] - 
-            dilation[dilation_shape[i]]*( kernel_size[kernel_shape[i]] - 1) - 1 ) / stride[stride_shape[i]])
-
-        return (reshape(0), reshape(1), reshape(2))
 
 def train_model(model : nn.Module, 
                 train_data: DataLoader, 
@@ -281,17 +185,31 @@ def train_model(model : nn.Module,
                 epochs : int,
                 loss_fn : nn.MSELoss):
 
-    batch_size = train_data.batch_size 
-    if (batch_size := train_data.batch_size) is None:
-        raise ValueError("Training DataLoader must have a batch size")
+    wandb.init(
+        # set the wandb project where this run will be logged
+        project="Torch-grids",
 
+        # track hyperparameters and run metadata
+        config={
+        "learning_rate": optimizer.state_dict()['param_groups'][0]['lr'],
+        "architecture": "CNN",
+        "dataset": "PDBbind",
+        "epochs": epochs,
+        }
+    )
     for epoch in range(epochs):
         # TRAINING STEP -----------------------------
-        avg_loss = 0
         running_loss = 0
         print(f"\nEPOCH {epoch+1}/{epochs}")
         model.train()
-        for X, y, w in train_data:
+        pad = 15
+        count = 0
+        total_train = len(train_data)
+        for i, (X, y, w) in enumerate(train_data):
+
+            pad_left = int(i*pad/total_train)
+            print(f"\rRunning Epoch... |{'':{'='}<{pad_left}}>{'':{' '}>{pad-pad_left-1}}|", end='')
+
             pred = model(X)
             loss = loss_fn(pred, y)
             optimizer.zero_grad()
@@ -299,7 +217,13 @@ def train_model(model : nn.Module,
             optimizer.step()
             running_loss += loss.item() * w.sum().item()
 
-        print(f"Train Loss: {running_loss/len(train_data):.4f}")
+            count += 1
+
+        loss = running_loss/total_train
+        wandb.log({"train loss": loss})
+        print('\n')
+        print(f"Train Loss: {loss:.4f}")
+
 
         # VALIDATION STEP ---------------------------
         model.eval()
@@ -310,9 +234,11 @@ def train_model(model : nn.Module,
                 vloss = loss_fn(vpred, yv)
                 running_vloss += vloss.item()
 
-        print(f"Validation Loss: {running_vloss/len(valid_data):.4f}")
+        vloss = running_loss/len(valid_data)
+        wandb.log({"valid loss": vloss})
+        print(f"Validation Loss: {vloss:.4f}")
 
-
+    wandb.finish()
 
 def test():
 
@@ -327,7 +253,7 @@ def test():
     epochs = 10
     loss_fn = nn.MSELoss()
     optimizer = torch.optim.Adam(cnn.parameters(), lr=learning_rate)
-    train, _, valid = create_datasets(device, 1, max_targets=100)
+    train, _, valid = create_datasets(device, batch_size, max_targets=100)
     train_model(cnn, train, valid, optimizer, epochs, loss_fn)
 
 
